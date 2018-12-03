e0c25d16b1a7b05f1d3052a23b34541dc916e30c
liyun-li <liyun-li@users.noreply.github.com>
2018-12-03 06:54:11 -0500

    Add Docker support

---
+++ src/build-and-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+name=`cat shodan/.env | grep DB_NAME | cut -d '=' -f2`
+user=`cat shodan/.env | grep DB_USER | cut -d '=' -f2`
+password=`cat shodan/.env | grep DB_PASS | cut -d '=' -f2`
+
+[ -z $name ] && echo 'DB name required' && exit 1
+[ -z $user ] && echo 'DB user required' && exit 1
+[ -z $password ] && echo 'DB password required' && exit 1
+
+cp -n docker-compose.yml docker-compose.yml.bak
+
+sed -i "s/<DB_NAME_HERE>/$name/g" docker-compose.yml
+sed -i "s/<DB_USER_HERE>/$user/g" docker-compose.yml
+sed -i "s/<DB_PASSWORD_HERE>/$password/g" docker-compose.yml
+sed -i "s/<DB_ROOT_PASSWORD_HERE>/`./shodan/pwgen.sh`/g" docker-compose.yml
+
+docker-compose up --build
